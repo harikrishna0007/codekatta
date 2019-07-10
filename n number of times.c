081cae551a5b45d98961ac4503f258a8088c8ec7
Hari Krishna .E <51771851+harikrishna0007@users.noreply.github.com>
2019-07-10 15:24:48 +0530

    Create n number of times.c

---
+++ n number of times.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    if(n>=0){
+    for(i=1;i<=n;i++){
+        printf("Hello\n");
+    }
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
