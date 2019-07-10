8f5c04646ba05374f1548d25934d6e20d4982ce6
Hari Krishna .E <51771851+harikrishna0007@users.noreply.github.com>
2019-07-10 14:52:07 +0530

    Create positive or negative.cpp

---
+++ positive or negative.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	if(num == 0)
+		cout << "Zero";
+	else if (num < 0)
+		cout << "Negative";
+	else
+		cout << "Positive";
+	return 0;
+}
