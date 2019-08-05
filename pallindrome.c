adb5a30a2e47b0b0360866579e476ffcd7fca393
Hari Krishna .E <51771851+harikrishna0007@users.noreply.github.com>
2019-08-05 19:33:06 +0530

    Create pallindrome.c

---
+++ pallindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  int n,r,sum=0,temp;
+  scanf("%d",&n);
+  temp=n;
+  while(n>0)
+  {
+    r=n%10;
+    sum=sum*10+r;
+    n=n/10;
+  }
+  n=temp;
+  if(n==sum)
+  {
+    printf("yes");
+  }
+    else
+    {
+      printf("no");
+    }
+  
+  return 0;
+}
