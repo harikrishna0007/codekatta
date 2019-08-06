799766faa2e70ea2c9b1b909adc9aff843fbc19f
Hari Krishna .E <51771851+harikrishna0007@users.noreply.github.com>
2019-08-06 17:00:52 +0530

    Create compare two strings.cpp

---
+++ compare two strings.cpp
@@ -0,0 +1,23 @@
+      #include<iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+   string s;
+   string k;
+   int count1=0,count2=0;
+   cin>>s>>k;
+   for(int i=0;i<s.length();i++){
+   	count1++;
+   }
+   for(int j=0;j<k.length();j++){
+   	count2++;
+   }
+   if(count1>=count2){
+   	cout<<s;
+   }
+   else{
+   	cout<<k;
+   }
+   
+}
