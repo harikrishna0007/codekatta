2b48df123678b92341ac26a8e917e5a1e16c8c0a
Hari Krishna .E <51771851+harikrishna0007@users.noreply.github.com>
2019-08-06 13:13:06 +0530

    Create count no of spaces in line.cpp

---
+++ count no of spaces in line.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int count=0;
+  string s;
+  getline(cin,s);
+  for(int i=0;i<s.length();i++){
+    if(s[i]==' '){
+      count++;
+    }
+  }
+  cout << count;
+}
