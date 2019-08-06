611ec7f84d251aed89e5ba7bf109d708c854ea44
Hari Krishna .E <51771851+harikrishna0007@users.noreply.github.com>
2019-08-06 16:21:16 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main(void) {
+int main() {
   int n,i,a=0,b=1,c;
-  scanf("%d",&n);
+  cin >> n;
   for(i=1;i<=n;i++){
-    printf("%d",b);
+    cout << b <<' ';
     c=a+b;
     a=b;
     b=c;
